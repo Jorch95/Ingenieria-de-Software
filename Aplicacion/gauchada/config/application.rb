03590ec3c35c91c2c1cdafcca2fa4f8e4e12ff84
trorik23 <trorik23@gmail.com>
2016-11-02 15:11:40 -0300

    Se agrega el archivo de configuracion de lenguaje

--- Aplicacion/gauchada/config/application.rb
+++ Aplicacion/gauchada/config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.i18n.default_locale = :es
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
