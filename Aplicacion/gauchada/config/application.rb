57c96bfe9aa45291ca124b696d135ecb84784fb7
trorik23 <trorik23@gmail.com>
2016-11-02 15:25:14 -0300

    Se agrego la columna de imagen

--- Aplicacion/gauchada/config/application.rb
+++ Aplicacion/gauchada/config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.i18n.default_locale = :es
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +24,7 @@ module Gauchada
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.i18n.default_locale = :es
   end
 end
--- Aplicacion/gauchada/db/schema.rb
+++ Aplicacion/gauchada/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102173806) do
+ActiveRecord::Schema.define(version: 20161102182313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161102173806) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "image_url"
   end
 
   add_index "favours", ["user_id"], name: "index_favours_on_user_id", using: :btree
