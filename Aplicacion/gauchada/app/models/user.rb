8edbc6ae2491d46a666ef2dad6bb6f412038fb2c
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-04 01:06:50 -0300

    Se agrego gemas y se arreglo la barra superior
    gemas:
    -country_select
    -font-awesome-rails
    
    Se siguio trabajando con el registro de usuarios
    Se adapto el inicio de sesion en la barra principal

--- Aplicacion/gauchada/app/models/user.rb
+++ Aplicacion/gauchada/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 7..128
   has_one :achievement
   has_many :purchases
   has_many :favours
