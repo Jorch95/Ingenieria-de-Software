1aa11536f771ce1d61a35e62b64e747a3244d59b
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-08 20:00:33 -0300

    Se modifico vistas controladores y configuraciones
    -Se modifico el controlador de la parte legal con links arreglados
    -Los estilos de la parte:
    	- Legal
    	- Aplication: Se cambio el fondo de footer.
    - Las vistas:
    	Devise: lo que es password, registro y secciones
    	Legal: porque habian estilos muy genericos y los especifique solo para lo legal porque puede
    		colisionar con las vistas de otras partes.
    	Aplication: agregando funcionalidad de usuario ante un loggeo.
    - Configuraciones:
    	Es parte de devise pero no esta terminado, es sobre el envio de mail ante una solcitud de de contraseña

--- Aplicacion/gauchada/config/environments/development.rb
+++ Aplicacion/gauchada/config/environments/development.rb
@@ -12,18 +12,29 @@ Rails.application.configure do
 
 
   #configuracion para la gema devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #####       config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '192.168.137.2', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'gauchadaunlp@gmail.com',
+      :password             => 'gauchadaadmin',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  
+    }
 
+  # Don't care if the mailer can't send.
+  #####       config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-
+  # Envia emails en el dessarollo web
+  config.action_mailer.perform_deliveries = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
