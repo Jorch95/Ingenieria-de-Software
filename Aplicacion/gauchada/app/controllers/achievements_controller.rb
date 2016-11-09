71feebf4899f901dfcef2e4c47a84c5618a7b87f
trorik23 <trorik23@gmail.com>
2016-11-09 17:49:43 -0300

    Se manejo la autorizacion de los logros
    Los accesos a las distintas acciones de los logros son diferentes dependiendo si sos:
    	usuario no registrado
    	usuario registrado
    	usuario registrado y admin

--- Aplicacion/gauchada/app/controllers/achievements_controller.rb
+++ Aplicacion/gauchada/app/controllers/achievements_controller.rb
@@ -1,4 +1,7 @@
 class AchievementsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :es_admin?, only: [:index, :destroy]
+
   def index
     order = params[:sort] || "rango"
     if order == "titulo"
@@ -8,10 +11,18 @@ class AchievementsController < ApplicationController
     end
     @achievements=Achievement.where(enable: true).order(ordering)
   end
+
   def destroy
     a=Achievement.find(params[:id])
     a.enable=false
     a.save
     redirect_to achievements_path
+  end
+
+  private
+  def es_admin?
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 end
