32fe80a26a7b4e57055abe1bbe90fd7f17eee521
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-09 17:35:29 -0300

    Se agrega una gema y se termina el modulo de recuperar contraseña
    La gema que se agrega es date_validator
    Ya se puede recuperar contraseña al menos dentro de localhost

--- Aplicacion/gauchada/app/controllers/application_controller.rb
+++ Aplicacion/gauchada/app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :apellido, :telefono, :fecha_nacimiento, :sexo, :pais ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :apellido, :telefono, :fecha_nacimiento, :sexo, :pais, :terminos_legales])
   end
 end
