3f61a1beae5bb1cdf3eda22694bbf46ea16ef4d0
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-09 21:05:53 -0300

    Se actualizo el seed.rb y se eliminaron las imagenes

--- Aplicacion/gauchada/db/seeds.rb
+++ Aplicacion/gauchada/db/seeds.rb
@@ -8,24 +8,27 @@
 
 puts 'Creo algunos usuarios'
 
-nacim = DateTime.new(2016, 11, 02) # Estandar ISO 8601: YYYY, MM, DD
-caduc = DateTime.new(2018, 6, 22)
+nacim = DateTime.new(1990, 11, 02) # Estandar ISO 8601: YYYY, MM, DD
+caduc = DateTime.new(1990, 6, 22)
 users = []
 4.times do |i|
-  users[i] = User.create(nombre: "Lalo#{i}", apellido: "Mir#{i}", password:"password",password_confirmation: "password", telefono: 534543, puntaje: 5, pais: "Argentina", sexo: true, email: "lalo#{i}@gmail.com", fecha_nacimiento: nacim, admin: false, tc_apellido: "Mir",
+  users[i] = User.create(nombre: "Lalo#{i}", apellido: "Mir#{i}", password:"password",password_confirmation: "password", telefono: "5345434444", puntaje: 5, pais: "Argentina", sexo: true, email: "lalo#{i}@gmail.com", fecha_nacimiento: nacim, admin: false, tc_apellido: "Mir",
     tc_nombre: "Lalo", tc_numero: 45434534, tc_pin: 43793400, tc_caducidad: caduc)
 end
 
-User.create(nombre: "nahuel", apellido: "ter", password:"123123123", password_confirmation: "123123123", telefono: 53345431, puntaje: 5, pais: "Argentina", sexo: true, email: "unlp93nahuelter@gmail.com", fecha_nacimiento: nacim, admin: true, tc_apellido: "Mir",
+User.create(nombre: "nahuel", apellido: "ter", password:"123123123", password_confirmation: "123123123", telefono: "12345678910", puntaje: 5, pais: "Argentina", sexo: true, email: "unlp93nahuelter@gmail.com", fecha_nacimiento: nacim, admin: true, tc_apellido: "Mir",
     tc_nombre: "Lalo", tc_numero: 45434534, tc_pin: 43793400, tc_caducidad: caduc)
 
-4.times do |f|
-  users[f].favours.create(titulo: "Ayuda #{f}", descripcion:" Descripcion #{f}", ciudad: "Alguna #{f}", fecha: DateTime.new(2016,11,04), image_url: "perro#{f}.jpg" )
+puts 'Creo algunos favores'
+
+  users[0].favours.create(titulo: "Ayuda 0", descripcion:" Descripcion 0", ciudad: "Alguna 0", fecha: DateTime.new(2016,11,04), image_url: "http://gdurl.com/Tt5L" )
+  users[1].favours.create(titulo: "Ayuda 1", descripcion:" Descripcion 1", ciudad: "Alguna 1", fecha: DateTime.new(2016,11,04), image_url: "http://gdurl.com/kt6L" )
+  users[2].favours.create(titulo: "Ayuda 2", descripcion:" Descripcion 2", ciudad: "Alguna 2", fecha: DateTime.new(2016,11,04), image_url: "http://gdurl.com/e_E4" )
+  users[3].favours.create(titulo: "Ayuda 3", descripcion:" Descripcion 3", ciudad: "Alguna 3", fecha: DateTime.new(2016,11,04), image_url: "http://gdurl.com/avof" )
   #puts users[f].valid?
   #users[f].errors.full_messages.each do |msg|
   #  puts msg
   #send
-end
 
 puts 'Creo algunos logros'
 5.times do |i|
