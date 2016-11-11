5c5a707e01fc09b430777dde970a91e7aad37d68
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-11 10:41:51 -0300

    Probando configuracion de mails en heroku v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,13 +88,14 @@ Rails.application.configure do
  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'nosrobaronelnombre.herokuapp.com' }
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.gmail.com",
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
-    :enable_starttls_auto => true
-  }
+config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "nosrobaronelnombre.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
 end
