3a50d39a5e397316db8418214a961a63b55be70d
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-11 11:05:27 -0300

    Mailer de devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'gauchadaunlp@gmail.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
