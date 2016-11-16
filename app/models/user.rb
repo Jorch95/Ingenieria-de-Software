143c77e51f6ad88c2e6c9f1e0ccf4b57d75c0f43
trorik23 <trorik23@gmail.com>
2016-11-16 09:34:09 -0300

    Se corrige la posibilidad de editar datos del usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,15 @@ class User < ActiveRecord::Base
 
 	validates :terminos_legales, acceptance: true
 	# :sexo, :pais
-	validates_presence_of :email, :password, :password_confirmation, :nombre, :apellido, :fecha_nacimiento,:telefono
-	validates :password, :password_confirmation, length: { minimum: 7 }
+
+	validates_presence_of :email, :nombre, :apellido, :fecha_nacimiento,:telefono
 	#validates :tc_pin, length: { minimum: 4, maximum: 4 }
 
-	validates :email, format: { with: /\A[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})\z/, message: ": caracteres permitidos: a-z, A-Z, 0-9, guiones, punto (.)" } 
+	validates :password, :password_confirmation, presence: true, length: {minimum: 7, maximum: 30}, on: :create
+	validates :password, :password_confirmation, length: {minimum: 7, maximum: 30}, on: :update, allow_blank: true
+
+
+	validates :email, format: { with: /\A[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})\z/, message: ": caracteres permitidos: a-z, A-Z, 0-9, guiones, punto (.)" }
 	validates_format_of :telefono, :with =>  /\d[0-9]\)*\z/ , :message => "Solamente numeros sin espacios"
 	validates :fecha_nacimiento,
 				date: { before: Proc.new { Time.now - 18.year }, message: 'Debes ser mayor de 18 años.' },
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   get 'legal/privacy'
 
   get "/" => "favours#index", :as => "root"
-
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
