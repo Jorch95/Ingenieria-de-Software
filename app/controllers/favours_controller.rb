39de4614b7c773325e980ec979954fae025f5c6b
Nahuel Ternouski <unlp93nahuelter@gmail.com>
2016-11-17 23:36:22 -0300

    Se avanzo con el filtro de los favores, solo falta busqueda de descripcion

--- app/controllers/favours_controller.rb
+++ app/controllers/favours_controller.rb
@@ -1,6 +1,6 @@
 class FavoursController < ApplicationController
   def index
-    	 @favours = Favour.all
+      @favours = inicializar_favor
   end
   def show
       @favour = Favour.find(params[:id])
@@ -12,4 +12,34 @@ class FavoursController < ApplicationController
         redirect_to action: "index"
       end
   end
+
+
+  def inicializar_favor 
+    if params[:filtrado_por].present? 
+        criterio = params[:criterio]
+
+        if params[:filtrado_por] == "Fecha decreciente"
+          return Favour.all.descendente()
+        elsif params[:filtrado_por] == "Fecha creciente"
+          return Favour.all.ascendente()
+        end
+
+        if criterio == ""
+          return Favour.all
+        elsif params[:filtrado_por] == "Descripcion"
+          #return Favour.descripcion(criterio)
+          #return Favour.includes(:resources).where("resources.resource_type_id = ?", @resource_type.id)
+          #return Favour.includes(:criterio).where("descripcion = ?", criterio)
+        elsif params[:filtrado_por] == "Titulo"
+          return Favour.titulo(criterio)
+        elsif params[:filtrado_por] == "Ciudad"
+          return Favour.ciudad(criterio)
+        elsif params[:filtrado_por] == "Provincia"
+          return Favour.provincia(criterio)
+        end
+    else
+        return Favour.all
+    end
+  end
+
 end
--- app/models/favour.rb
+++ app/models/favour.rb
@@ -3,5 +3,14 @@ class Favour < ActiveRecord::Base
   has_many :requests
   has_many :comments
   has_one :grade
-  default_scope -> {order("created_at desc")}
+
+  scope :provincia, -> (provincia) { where provincia: provincia }
+  scope :titulo, -> (titulo) { where titulo: titulo }
+  scope :descripcion, -> (descripcion) { where descripcion: descripcion }
+  #scope :descripcion, -> (descripcion) { where( ["keyword LIKE :descripcion", {:descripcion => "%#{descripcion}%"}] ) }
+
+  scope :ciudad, -> (ciudad) { where ciudad: ciudad }
+  scope :descendente, -> {order("fecha desc")}
+  scope :ascendente, -> {order("fecha asc")}
+
 end
