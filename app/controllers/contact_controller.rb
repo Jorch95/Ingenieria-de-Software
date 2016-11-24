11f28a170ccbd22737d84f7c5ab618be44ab57af
trorik23 <trorik23@gmail.com>
2016-11-24 10:10:21 -0300

    Mas notificaciones

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,5 +1,10 @@
 class ContactController < ApplicationController
+  before_action :authenticate_user!
   def contact
-    @user=User.find(params[:id])
+    if current_user.id==Request.find(params[:request_id]).favour.user.id
+    @user=Request.find(params[:request_id]).user
+  else
+    redirect_to root_path
+  end
   end
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,5 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_user!
   def new
     @request=Request.new
     @favour=Favour.find(params[:favour_id])
@@ -8,6 +9,7 @@ class RequestsController < ApplicationController
     @favour = Favour.find(params[:favour_id])
     @request= current_user.requests.new(descripcion: params[:request][:descripcion], fecha: params[:request][:fecha], favour_id: @favour.id)
     if @request.save
+      @favour.user.notifications.create(texto: "Tienes una nueva solicitud de ayuda para el favor "+@favour.titulo)
       redirect_to favour_url(@favour)
       flash[:notice]="La solicitud se ha enviado correctamente"
     else
@@ -48,8 +50,8 @@ class RequestsController < ApplicationController
     #debe eliminar el resto
     r.favour.requests.where.not(id: params[:id]).destroy_all
     r.favour.update(aceptado: true)
-    r.user.notifications.create(texto: "Se ha aceptado tu solicitud al favor "+r.favour.titulo)
-    redirect_to contact_path(id: r.user.id)
+    r.user.notifications.create(texto: "Se ha aceptado tu solicitud al favor "+r.favour.titulo+". El usuario ya posee tus datos de contacto")
+    redirect_to contact_path(request_id: r.id)
   end
 
 end
