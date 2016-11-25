6371bfc64b614287d5f0580861ec9755e4f43072
trorik23 <trorik23@gmail.com>
2016-11-24 21:16:27 -0300

    Super mejoras

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,11 @@
 class NotificationsController < ApplicationController
   def index
     @notifications=current_user.notifications
+    if @notifications.any?
+    else
+      flash[:notice]="No tienes niguna notificacion"
+      redirect_to :back
+    end
   end
 
   def destroy
